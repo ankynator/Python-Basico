387c4fe08f0928c27e7c23e607cdfca3485e1112
ankynator <ankynator@gmail.com>
2021-01-27 22:14:25 -0500

    implementando un ciclo FOR

---
+++ for.py
@@ -0,0 +1,13 @@
+# contador = 0
+
+# while contador < 10:
+#     print(contador)
+#     contador += 1
+
+a = list(range(10))
+
+# for contador in a:
+#     print(contador)
+
+for i in range(1, 11):
+    print(i*9)
